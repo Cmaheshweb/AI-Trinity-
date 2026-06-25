b0c065910ebc680d1a94f9efc146e088365dff95
Mahesh mhaske <mahesh.mhaske0369@gmail.com>
2026-06-25 12:35:35 +0530

    Create backend/src/auth/dto/register.dto.ts

---
+++ backend/backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+}
